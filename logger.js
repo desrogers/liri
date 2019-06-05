137a7a693b0ea9f3f98b1c297caa4c1c69b84e4e
dezroge <desmondrogers2@gmail.com>
2019-06-04 19:09:19 -0500

    Move logs to /logs

--- logger.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-
-module.exports = function(input){
-    fs.appendFile("log.txt", input, function(err){
-        if (err){
-           return console.log(err);
-        }
-    })
-}
